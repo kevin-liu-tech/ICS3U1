442a8e2e318a374231439d1455a32a5b940e04a5
Kevin Liu <kevin.liu.tech@gmail.com>
2020-10-12 17:35:33 -0400

    enter

--- BooleanTest.java
+++ BooleanTest.java
@@ -12,8 +12,8 @@ public class BooleanTest {
         boolean c = keyboard.nextBoolean();
         System.out.println("Please enter your d value: ");
         boolean d = keyboard.nextBoolean();
-        boolean endgate = ((a && b || c && d) || (c && a || a && d)) || ((c && a || a && d) || (d && b || b && c));
-        System.out.println("The output value based on the truth table is: " + endgate);
+        boolean booleanLogicGate = ((a && b || c && d) || (c && a || a && d)) || ((c && a || a && d) || (d && b || b && c));
+        System.out.println("The output value based on the truth table is: " + booleanLogicGate);
         keyboard.close();
     }
 
