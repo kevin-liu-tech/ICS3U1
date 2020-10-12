6346ed6a64712c2a4f36df52745679d3ef879a09
Kevin Liu <kevin.liu.tech@gmail.com>
2020-10-12 16:49:58 -0400

    Enter

--- BooleanTest.java
+++ BooleanTest.java
@@ -4,19 +4,28 @@ public class BooleanTest {
     
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
-        boolean a = false;
-        boolean b = false; 
-        boolean c = false;
-        boolean d = false; 
         System.out.println("Please enter your a value: ");
-        a = keyboard.nextBoolean();
+        boolean a = keyboard.nextBoolean();
         System.out.println("Please enter your b value: ");
-        b = keyboard.nextBoolean();
+        boolean b = keyboard.nextBoolean();
         System.out.println("Please enter your c value: ");
-        c = keyboard.nextBoolean();
+        boolean c = keyboard.nextBoolean();
         System.out.println("Please enter your d value: ");
-        d = keyboard.nextBoolean();
-        System.out.println("The output value based on the truth table is: ");
+        boolean d = keyboard.nextBoolean();
+        boolean gate1 = a&&b;
+        boolean gate2 = c&&d;
+        boolean gate3 = c&&a;
+        boolean gate4 = a&&d;
+        boolean gate5 = b&&d;
+        boolean gate6 = b&&c;
+        boolean gate7 = gate1 || gate2;
+        boolean gate8 = gate3 || gate4;
+        boolean gate9 = gate5 || gate6;
+        boolean gate10 = gate7 || gate8;
+        boolean gate11 = gate8 || gate9;
+        boolean gate12 = gate10 || gate11;
+        System.out.println("The output value based on the truth table is: " + gate12);
+        keyboard.close();
     }
 
 }
