a04d91644ed9d396f96ff54c9de752cbb9f82364
kevin-liu-tech <55010038+kevin-liu-tech@users.noreply.github.com>
2020-10-21 17:23:32 -0400

    ENTER

---
+++ NewtonsMethod.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NewtonsMethod {
+    public static double f(double a) {
+        return 6 * Math.pow(a, 4) - 13 * Math.pow(a, 3) - 18 * Math.pow(a, 2) + 7 * a + 6;
+    }
+    public static double d(double a) {
+        return 24 * Math.pow(a, 3) - 39 * Math.pow(a, 2) - 36 * a + 7;
+    }
+    public static void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to Newton's Method. Please enter your initial root guess!");
+        double x0 = keyboard.nextDouble();
+        double x1 = x0 - f(x0)/d(x0);
+        boolean answer = false;
+        while ((x1 - x0) > 0.0001) {   
+            x0 = x1;
+            x1 = x0 - f(x0)/d(x0);
+        }
+        keyboard.close();
+    }
+}
