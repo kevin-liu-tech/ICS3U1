79202a5cc8fca467edc94527fba2738e5bc9baba
Kevin Liu <kevin.liu.tech@gmail.com>
2020-10-22 12:36:11 -0400

    Enter

--- NewtonsMethod.java
+++ NewtonsMethod.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 public class NewtonsMethod {
     public static void main (String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to Newton's Method. Please enter your initial root guess!");
-        double x0 = keyboard.nextDouble();
-        double x1 = x0 - f(x0)/d(x0);
-        while ((x1 - x0) > 0.0001) {   
-            x0 = x1;
-            x1 = x0 - f(x0)/d(x0);
+        double guess = 0.0;
+        double newGuess = 0.0;
+
+        loop:
+        while (true) {
+            System.out.println("Welcome to Newton's Method. Please enter your initial root guess!");
+            guess = keyboard.nextDouble();
+            
+            newGuess = guess - (6 * Math.pow(guess, 4) - 13 * Math.pow(guess, 3) - 18 * Math.pow(guess, 2) + 7 * guess + 6)/(24 * Math.pow(guess, 3) - 39 * Math.pow(guess, 2) - 36 * guess + 7);
+            
+            while (Math.abs(guess - newGuess) > 0.00001) {   
+                guess = newGuess;
+                newGuess = guess - (6 * Math.pow(guess, 4) - 13 * Math.pow(guess, 3) - 18 * Math.pow(guess, 2) + 7 * guess + 6)/(24 * Math.pow(guess, 3) - 39 * Math.pow(guess, 2) - 36 * guess + 7);
+            }
+            System.out.println("The root closest to your guess is:" + newGuess);
+            
+            System.out.println("Would you like to Guess again?: (1 - yes, or 2 - no");
+            if (keyboard.nextInt() == 2) {
+                System.out.println("Thank you for using this program!");
+                break loop;
+            }
         }
-        System.out.println("The root closest to your guess is:" + x1);
         keyboard.close();
     }
 }
--- PIApproximation.java
+++ PIApproximation.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 public class PIApproximation {
     public static void main (String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to the PI Approximation!");
-        int nValue = keyboard.nextInt();
-        
+        System.out.println("Welcome to the PI Approximation! Using the Gregory–Leibniz series, this program approximates the value of PI!");
+        loop:
+        while (true) {
+            System.out.println("Please enter your n value:");
+            int maxIterations = keyboard.nextInt();
+            double equation = 0;
+            double finalOutputSum = 0;
+    
+            for (int i = 0; i < maxIterations; i ++) {
+                equation = Math.pow(-1, i)/(i * 2 + 1);
+                finalOutputSum += equation;
+            }
+            System.out.println("The output based on your n-value is " + finalOutputSum * 4);
+            System.out.println("Would you like to guess again? (Press 1 for yes or any other number for no): ");
+            if (keyboard.nextInt() != 1) {
+                System.out.println("Thank you for using the program. Have a good day!");
+                break loop;
+            }
+        }
+        keyboard.close();
     }
 }
