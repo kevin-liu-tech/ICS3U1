eb5035850f08df7931fbcf49f42d1f5611a83388
Kevin Liu <kevin.liu.tech@gmail.com>
2020-10-22 22:26:25 -0400

    Enter

--- PIApproximation.java
+++ PIApproximation.java
@@ -10,13 +10,18 @@ public class PIApproximation {
             int maxIterations = keyboard.nextInt();
             double equation = 0;
             double finalOutputSum = 0;
-    
-            for (int i = 0; i < maxIterations; i ++) {
-                equation = Math.pow(-1, i)/(i * 2 + 1);
-                finalOutputSum += equation;
+            
+            if (maxIterations < 0) {
+                System.out.println("Error, please enter a valid positive integer.");
+                System.out.println("Would you like to guess again? (Press 1 for yes or any other number for no): ");
+            } else {
+                for (int i = 0; i < maxIterations; i ++) {
+                    equation = Math.pow(-1, i)/(i * 2 + 1);
+                    finalOutputSum += equation;
+                }
+                System.out.println("The output based on your n-value is " + finalOutputSum * 4);
+                System.out.println("Would you like to guess again? (Press 1 for yes or any other number for no): ");
             }
-            System.out.println("The output based on your n-value is " + finalOutputSum * 4);
-            System.out.println("Would you like to guess again? (Press 1 for yes or any other number for no): ");
             if (keyboard.nextInt() != 1) {
                 System.out.println("Thank you for using the program. Have a good day!");
                 break loop;
